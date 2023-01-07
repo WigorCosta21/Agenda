5d6882666a3685a4090ebd3144c1aa6929f5a2bf
Wigor <wigor21@hotmail.com>
2023-01-07 18:25:07 -0300

    Login and logout session

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,30 +1,58 @@
-const { async } = require("regenerator-runtime");
-const Login = require("../models/LoginModels");
+const Login = require('../models/LoginModels');
 
 exports.index = (req, res) => {
-  res.render("login");
+  if(req.session.user) return res.render('login-logado');
+  return res.render('login');
 };
 
-exports.register = async (req, res) => {
+exports.register = async function(req, res) {
   try {
     const login = new Login(req.body);
     await login.register();
 
-    if (login.errors.length > 0) {
-      req.flash("errors", login.errors);
-      req.session.save(function () {
-        return res.redirect("/login");
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('back');
       });
       return;
     }
 
-    req.flash("success", 'Seu usuário foi criado com sucesso');
-      req.session.save(function () {
-        return res.redirect("/login");
-      });
-
-  } catch (e) {
+    req.flash('success', 'Seu usuário foi criado com sucesso.');
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+  } catch(e) {
     console.log(e);
-    return res.render("404");
+    return res.render('404');
   }
+};
+
+exports.login = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('back');
+      });
+      return;
+    }
+
+    req.flash('success', 'Você entrou no sistema.');
+    req.session.user = login.user;
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.logout = function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
 };
