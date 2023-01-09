aefdb3db2a241a24548e917c80d6b9b7cf39a342
Wigor <wigor21@hotmail.com>
2023-01-08 21:09:48 -0300

    registering contact

--- routes.js
+++ routes.js
@@ -1,14 +1,24 @@
 const express = require('express')
-const route = express.Router()
+const router = express.Router()
 const homeController = require('./src/controllers/homeController')
 const loginController = require('./src/controllers/loginController')
+const contatoController = require('./src/controllers/contatoController')
+
+const { loginRequired } = require('./src/middlewares/middleware')
 
 //Rotas da home
-route.get('/', homeController.index)
+router.get('/', homeController.index)
 
 //Rotas de login
-route.get('/login', loginController.index)
-route.post('/login/register', loginController.register)
-route.post('/login/login', loginController.login)
-route.get('/login/logout', loginController.logout)
-module.exports = route
+router.get('/login', loginController.index)
+router.post('/login/register', loginController.register)
+router.post('/login/login', loginController.login)
+router.get('/login/logout', loginController.logout)
+
+//Rotas de contatos
+
+router.get('/contato', loginRequired ,contatoController.index)
+router.get('/contato/:id', loginRequired ,contatoController.edit)
+router.post('/contato/register', loginRequired ,contatoController.register)
+
+module.exports = router
